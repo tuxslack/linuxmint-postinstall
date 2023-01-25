84d47b8fe5d877ba43c8e37ab0205b81026fb3ea
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2023-01-25 13:57:55 -0300

    Update active-directory.sh

--- active-directory.sh
+++ active-directory.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
+sudo apt -y update
+sudo apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
+
 DOMINIO=$(\
     dialog --erase-on-exit --no-cancel --title "Configurar Active Directory"\
         --inputbox "Insira o domínio:" 8 40\
@@ -11,17 +15,6 @@ USUARIO=$(\
         --inputbox "Insira o usuário:" 8 40\
     3>&1 1>&2 2>&3 3>&- \
 )
-
-dialog --erase-on-exit --yesno "Deseja adicionar um grupo do domínio ao arquivo sudoers?" 8 60
-CONFIGURAR_SUDO=$?
-case $CONFIGURAR_SUDO in
-    0) GRUPO=$(dialog --erase-on-exit --no-cancel --title "Configurar Active Directory" --inputbox "Insira o grupo:" 8 40 3>&1 1>&2 2>&3 3>&-) ; sudo sed -i "/^%sudo.*ALL*/a %$GRUPO   ALL=(ALL:ALL) ALL" /etc/sudoers;;
-    1) echo "Você escolheu não adicionar grupo algum ao arquivo sudoers";;
-    255) echo "[ESC] key pressed.";;
-esac
-
-sudo apt -y update
-sudo apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
 
 ### Comandos TESTE
 echo "sudo realm join -U $USUARIO $DOMINIO"
@@ -45,3 +38,11 @@ sudo pam-auth-update
 sudo systemctl restart sssd
 
 echo "Adicionado ao domínio $DOMINIO com sucesso!"
+
+dialog --erase-on-exit --yesno "Deseja adicionar um grupo do domínio ao arquivo sudoers?" 8 60
+CONFIGURAR_SUDO=$?
+case $CONFIGURAR_SUDO in
+    0) GRUPO=$(dialog --erase-on-exit --no-cancel --title "Configurar Active Directory" --inputbox "Insira o grupo:" 8 40 3>&1 1>&2 2>&3 3>&-) ; sudo sed -i "/^%sudo.*ALL*/a %$GRUPO   ALL=(ALL:ALL) ALL" /etc/sudoers;;
+    1) echo "Você escolheu não adicionar grupo algum ao arquivo sudoers";;
+    255) echo "[ESC] key pressed.";;
+esac
