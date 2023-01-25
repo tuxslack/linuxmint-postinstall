86d8456af7f6ce865191e0966088601e4c2ecdad
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2023-01-25 14:53:13 -0300

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://github.com/thiagoneo/lmde-postinstall/archive/refs/tags/2023-01-25-testing.tar.gz
-tar -xzvf 2023-01-25-testing.tar.gz
-cd lmde-postinstall-2023-01-25-testing/
+wget https://github.com/thiagoneo/lmde-postinstall/archive/refs/tags/2023-01-25.tar.gz
+tar -xzvf 2023-01-25.tar.gz
+cd lmde-postinstall-2023-01-25/
 chmod +x *.sh
 
 bash script.sh
