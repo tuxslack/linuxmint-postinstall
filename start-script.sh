1957aa920d0bcc6e0526065c4a3f8d7e8250a1b9
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2023-02-14 16:15:01 -0300

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://github.com/thiagoneo/linuxmint-postinstall/archive/refs/tags/21.1.3.tar.gz
-tar -xzvf 21.1.3.tar.gz
-cd linuxmint-postinstall-21.1.3/
+wget https://github.com/thiagoneo/linuxmint-postinstall/archive/refs/tags/21.1.4.tar.gz
+tar -xzvf 21.1.4.tar.gz
+cd linuxmint-postinstall-21.1.4/
 chmod +x *.sh
 
 bash script.sh
