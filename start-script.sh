edccf6eb761c23d877cca727b53842729f6cdedf
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2023-02-13 23:11:27 -0300

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://github.com/thiagoneo/lmde-postinstall/archive/refs/tags/21.1.0.tar.gz
+wget https://github.com/thiagoneo/linuxmint-postinstall/archive/refs/tags/21.1.0.tar.gz
 tar -xzvf 21.1.0.tar.gz
-cd lmde-postinstall-21.1.0/
+cd linuxmint-postinstall-21.1.0/
 chmod +x *.sh
 
 bash script.sh
