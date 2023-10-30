74e60258df0dca6ce337113965c41e56061ad161
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2023-10-30 11:04:54 -0300

    Update active_directory.sh

--- active_directory.sh
+++ active_directory.sh
@@ -13,6 +13,37 @@ if ! dpkg -s dialog >/dev/null 2>&1; then
     apt install -y dialog
 fi
 
+altera_dns() {
+    DNS1=$(\
+            dialog --no-cancel --title "DNS primário"\
+                --inputbox "Insira o servidor DNS primário:" 8 40\
+            3>&1 1>&2 2>&3 3>&- \
+        )
+    
+    DNS2=$(\
+            dialog --no-cancel --title "DNS secundário"\
+                --inputbox "Insira o servidor DNS secundário (opcional):" 8 40\
+            3>&1 1>&2 2>&3 3>&- \
+        )
+    
+    IFS=$'\n'
+    
+    for CONN_NAME in $(nmcli --fields NAME --terse connection show)
+    do
+        sudo nmcli connection modify  "$CONN_NAME" ipv4.ignore-auto-dns true
+        sudo nmcli connection modify  "$CONN_NAME" ipv4.dns "${DNS1} ${DNS2}"
+        sudo nmcli connection down "$CONN_NAME"
+        sudo nmcli connection up "$CONN_NAME"
+    done
+}
+
+dialog_info() {
+    dialog --erase-on-exit --title "Aviso" --msgbox 'A seguir, insira o servidor DNS para resolver o domínio. Normalmente, o IP do servidor DNS é o mesmo do Controlador de Domínio, a menos que sejam servidores distintos.' 8 60
+}
+
+dialog_info
+altera_dns
+
 DOMINIO=$(\
     dialog --no-cancel --title "Configurar domínio Active Directory"\
         --inputbox "Insira o domínio:" 8 45\
@@ -39,18 +70,10 @@ echo $SENHA | realm join -U $USUARIO $DOMINIO
 
 STATUS=$?
 
-bash -c "cat > /usr/share/pam-configs/mkhomedir" <<EOF
-Name: activate mkhomedir
-Default: yes
-Priority: 900
-Session-Type: Additional
-Session:
-        required                        pam_mkhomedir.so umask=0022 skel=/etc/skel
-EOF
-
-dialog --title "Aviso" --msgbox "Na próxima tela você deverá marcar a opção 'activate mkhomedir', para que as pastas dos usuários sejam criadas automaticamente. Caso contrário, não será possível fazer login no ambiente gráfico!" 9 60
-
-pam-auth-update
+# Habilitar criacao automatica de pastas de usuarios ao fazer logon
+if ! grep -q "session.*required.*pam_mkhomedir.so.*umask.*" /etc/pam.d/common-session; then
+    echo 'session required                        pam_mkhomedir.so umask=0027 skel=/etc/skel' >> /etc/pam.d/common-session
+fi
 
 # Permitir fazer login sem a necessidade de acrescentar o "@dominio" ao nome de usuário:
 sed -i "s/use_fully_qualified_names = True/use_fully_qualified_names = False/g" /etc/sssd/sssd.conf
