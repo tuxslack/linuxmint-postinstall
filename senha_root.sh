2b5506b6b418baf5aadbcde82b954b40bd25a22e
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2024-01-19 23:00:02 -0300

    Update senha_root.sh

--- senha_root.sh
+++ senha_root.sh
@@ -12,30 +12,28 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-TITULO_DIALOG="Definir senha do root"
-
 ALT=1
 while [ $ALT = 1 ]
 do
     SENHA_ROOT=$(\
-        dialog --no-cancel --title ${TITULO_DIALOG}\
+        dialog --no-cancel --title "Definir senha do root"\
             --insecure --clear --passwordbox "Insira uma senha para o usuário root:" 8 45\
         3>&1 1>&2 2>&3 3>&- \
     )
     CONFIRMA_SENHA_ROOT=$(\
-        dialog --no-cancel --title ${TITULO_DIALOG}\
+        dialog --no-cancel --title "Definir senha do root"\
             --insecure --clear --passwordbox "Confirme a senha, digitando-a mais uma vez:" 8 45\
         3>&1 1>&2 2>&3 3>&- \
     )
     if [ $SENHA_ROOT = $CONFIRMA_SENHA_ROOT ]; then
         ALT=0
     else
-        dialog --no-cancel --title ${TITULO_DIALOG}\
+        dialog --no-cancel --title "Definir senha do root"\
             --msgbox "As senhas não são iguais. Tente novamente." 6 45
     fi
 done
 echo "root:$SENHA_ROOT" | chpasswd
 if [ $? = 0 ]; then
-    dialog --no-cancel --title ${TITULO_DIALOG}\
+    dialog --no-cancel --title "Definir senha do root"\
             --msgbox "Senha alterada com sucesso!" 6 40
 fi
