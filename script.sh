f634541941e46209524882a9628666af521ce9e3
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2024-05-04 10:11:53 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -112,6 +112,7 @@ sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/lightdm/ /etc/
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/skel/ /etc/
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/dconf/ /etc/
+dconf update
 \cp -rf "${SCR_DIRECTORY}"/system-files/usr/share/ukui-greeter/ /usr/share/
 \cp "${SCR_DIRECTORY}"/system-files/etc/default/grub /etc/default/grub
 echo "vm.swappiness=25" | tee -a /etc/sysctl.conf
