a50b951e70bbac63577458dfcc325decb8caae45
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2024-05-05 21:25:39 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -112,6 +112,8 @@ sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/lightdm/ /etc/
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/skel/ /etc/
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/dconf/ /etc/
+chmod 755 /etc/dconf/profile/
+chmod 755 /etc/dconf/db/local.d/
 dconf update
 \cp -rf "${SCR_DIRECTORY}"/system-files/usr/share/ukui-greeter/ /usr/share/
 \cp "${SCR_DIRECTORY}"/system-files/etc/default/grub /etc/default/grub
